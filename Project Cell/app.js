c5bf56ebcef1f851950c13bf2afb29f4d404a46a
hlyndon12 <53565325+hlyndon12@users.noreply.github.com>
2019-12-26 13:54:08 +0530

    Add files via upload

---
+++ Project Cell/app.js
@@ -0,0 +1,9 @@
+
+/*$(window).scroll(function(){
+  $(".top").css("opacity", 1 - $(window).scrollTop() / 600);
+});
+$(window).scroll(function(){
+  $(".mid").css("opacity", 1.7 - $(window).scrollTop() / 1000);
+});*/
+
+
